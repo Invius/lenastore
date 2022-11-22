6af4400d5f724779d2130a2c2ec32898bdd22a3a
Invius <marco.fernandes.inf10@hotmail.com>
2022-11-22 00:40:23 +0000

    Updates

---
+++ precache-manifest.4223e0090a8d1f08f5c1dd356577d623.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9706f8fcc96aece529f4a4c220e8138c",
+    "url": "/lenastore/index.html"
+  },
+  {
+    "revision": "98e36140dca9fc70f7cc",
+    "url": "/lenastore/static/css/1.d6802d45.chunk.css"
+  },
+  {
+    "revision": "8693f3384e86cecd08c7",
+    "url": "/lenastore/static/css/10.f5fe5e34.chunk.css"
+  },
+  {
+    "revision": "f745dab92330c2985249",
+    "url": "/lenastore/static/css/11.1e901936.chunk.css"
+  },
+  {
+    "revision": "7b299d7839c7846d356e",
+    "url": "/lenastore/static/css/12.930cbf77.chunk.css"
+  },
+  {
+    "revision": "38df3a785bc68988764d",
+    "url": "/lenastore/static/css/13.bc624b57.chunk.css"
+  },
+  {
+    "revision": "3125175e3457c65f0cfd",
+    "url": "/lenastore/static/css/14.5c616bdf.chunk.css"
+  },
+  {
+    "revision": "284bc0c838360d30989f",
+    "url": "/lenastore/static/css/15.27b9b4f4.chunk.css"
+  },
+  {
+    "revision": "a7565241934575483f52",
+    "url": "/lenastore/static/css/16.3bb66eef.chunk.css"
+  },
+  {
+    "revision": "e49dd923f02c68e3be93",
+    "url": "/lenastore/static/css/17.c862c410.chunk.css"
+  },
+  {
+    "revision": "4eb1c862c0429eedd759",
+    "url": "/lenastore/static/css/4.d5db0a6a.chunk.css"
+  },
+  {
+    "revision": "c28f90bf824d1094daeb",
+    "url": "/lenastore/static/css/5.601c81a8.chunk.css"
+  },
+  {
+    "revision": "d1f2118b8c5af99b6981",
+    "url": "/lenastore/static/css/6.3bb66eef.chunk.css"
+  },
+  {
+    "revision": "f3351419e5c6637c378d",
+    "url": "/lenastore/static/css/main.19dc44c8.chunk.css"
+  },
+  {
+    "revision": "da550c8317881683ab7e",
+    "url": "/lenastore/static/js/0.48ce30ae.chunk.js"
+  },
+  {
+    "revision": "98e36140dca9fc70f7cc",
+    "url": "/lenastore/static/js/1.df841b5a.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/lenastore/static/js/1.df841b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8693f3384e86cecd08c7",
+    "url": "/lenastore/static/js/10.be1b5922.chunk.js"
+  },
+  {
+    "revision": "cb257e125d374168cc7366e69bceb37f",
+    "url": "/lenastore/static/js/10.be1b5922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f745dab92330c2985249",
+    "url": "/lenastore/static/js/11.2299d5d4.chunk.js"
+  },
+  {
+    "revision": "7b299d7839c7846d356e",
+    "url": "/lenastore/static/js/12.f3421ead.chunk.js"
+  },
+  {
+    "revision": "38df3a785bc68988764d",
+    "url": "/lenastore/static/js/13.e6eb9c1a.chunk.js"
+  },
+  {
+    "revision": "3125175e3457c65f0cfd",
+    "url": "/lenastore/static/js/14.ffbdbe9c.chunk.js"
+  },
+  {
+    "revision": "284bc0c838360d30989f",
+    "url": "/lenastore/static/js/15.17df53c1.chunk.js"
+  },
+  {
+    "revision": "a7565241934575483f52",
+    "url": "/lenastore/static/js/16.ea111e8a.chunk.js"
+  },
+  {
+    "revision": "e49dd923f02c68e3be93",
+    "url": "/lenastore/static/js/17.a0534b9a.chunk.js"
+  },
+  {
+    "revision": "5553b6b5c6631f80158b",
+    "url": "/lenastore/static/js/18.ca9901dc.chunk.js"
+  },
+  {
+    "revision": "5886ba878c34c816bca9",
+    "url": "/lenastore/static/js/19.ac5c414c.chunk.js"
+  },
+  {
+    "revision": "cd46dabda49bff9efad3",
+    "url": "/lenastore/static/js/2.6e04b678.chunk.js"
+  },
+  {
+    "revision": "eb8a92a1ecd78c1d560d",
+    "url": "/lenastore/static/js/20.af01c648.chunk.js"
+  },
+  {
+    "revision": "9c9dbb101b987ddd9ea4",
+    "url": "/lenastore/static/js/21.d114927d.chunk.js"
+  },
+  {
+    "revision": "0958a8e59921fd6ee749",
+    "url": "/lenastore/static/js/22.4cc8f8fc.chunk.js"
+  },
+  {
+    "revision": "5142443377d610a99f8e",
+    "url": "/lenastore/static/js/23.3a665477.chunk.js"
+  },
+  {
+    "revision": "8429a357cb2e0c24ea43",
+    "url": "/lenastore/static/js/24.ab0aa3d0.chunk.js"
+  },
+  {
+    "revision": "48c67dbb68e9a2495199",
+    "url": "/lenastore/static/js/25.266ec146.chunk.js"
+  },
+  {
+    "revision": "314d3abebba18dbe5f6a",
+    "url": "/lenastore/static/js/3.c2d3a338.chunk.js"
+  },
+  {
+    "revision": "161232c254b16727d2c7e58393eddbb8",
+    "url": "/lenastore/static/js/3.c2d3a338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb1c862c0429eedd759",
+    "url": "/lenastore/static/js/4.3c4c9f22.chunk.js"
+  },
+  {
+    "revision": "c28f90bf824d1094daeb",
+    "url": "/lenastore/static/js/5.59666e8d.chunk.js"
+  },
+  {
+    "revision": "d1f2118b8c5af99b6981",
+    "url": "/lenastore/static/js/6.209a0b01.chunk.js"
+  },
+  {
+    "revision": "c016c6cbc9ce3476f2e5",
+    "url": "/lenastore/static/js/7.8522641b.chunk.js"
+  },
+  {
+    "revision": "f3351419e5c6637c378d",
+    "url": "/lenastore/static/js/main.d869dc22.chunk.js"
+  },
+  {
+    "revision": "46c29d1c8d89f9e0a004",
+    "url": "/lenastore/static/js/runtime-main.b3a39a0c.js"
+  },
+  {
+    "revision": "75ffb11b743a8f5994e7d311eceb4c51",
+    "url": "/lenastore/static/media/arrow-counterclockwise.75ffb11b.svg"
+  },
+  {
+    "revision": "a7a6201750dacf4a6d7447c305a39fb8",
+    "url": "/lenastore/static/media/bug-fill.a7a62017.svg"
+  },
+  {
+    "revision": "1fbfd4bcffccb94e8e8a5ea70616b296",
+    "url": "/lenastore/static/media/lg.1fbfd4bc.woff"
+  },
+  {
+    "revision": "2ec2cb2199d4d881e6a6ad86690f6add",
+    "url": "/lenastore/static/media/lg.2ec2cb21.svg"
+  },
+  {
+    "revision": "f4292655f93dd12d9b8e4fc067ef2489",
+    "url": "/lenastore/static/media/lg.f4292655.ttf"
+  },
+  {
+    "revision": "bd93a31721c1ac0c3f709b6616873437",
+    "url": "/lenastore/static/media/life-preserver.bd93a317.svg"
+  },
+  {
+    "revision": "174272f35101da9f303c335ce3250141",
+    "url": "/lenastore/static/media/search.174272f3.svg"
+  },
+  {
+    "revision": "734c483101ea9c220e2d499267528e04",
+    "url": "/lenastore/static/media/truck.734c4831.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a18cef0243b9a4be4b851f9cac2cc33.js"
+  "/lenastore/precache-manifest.4223e0090a8d1f08f5c1dd356577d623.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lenastore/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
